7d95851da435f4b10a96f9551f69986af2026215
Dgilca <dgilca@hiberus.com>
2023-06-21 09:04:31 +0200

    initial commit

---
+++ src/test/java/com/ngts/NGTS/NgtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngts.NGTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
